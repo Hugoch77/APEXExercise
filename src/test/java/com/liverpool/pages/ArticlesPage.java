0b39d2283b1501c02ad6d354159c8fe1c9fb0f69
Hugo <hug_alb_97@hotmail.com>
2026-01-07 14:51:29 -0600

    Fix comment formatting in ArticlesPage.java

--- src/test/java/com/liverpool/pages/ArticlesPage.java
+++ src/test/java/com/liverpool/pages/ArticlesPage.java
@@ -8,7 +8,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
-
+//coments
 import utilities.Utils;
 
 public class ArticlesPage {
